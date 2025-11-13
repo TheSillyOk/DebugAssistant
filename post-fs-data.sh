cf0f1a6d7f3dcc38a7243a4a75748c606e1e346c
TheSillyOk <priv.ld@proton.me>
2025-11-13 15:54:44 -0300

    improve: make logs based on the most recent file rather than naming; update: make DA create its own dir for logs; add: automatic copy of redacted files to /sdcard/Download; improve: make action.sh echo empty spaces for easier readability
    Signed-off-by: TheSillyOk <priv.ld@proton.me>

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -3,11 +3,14 @@
 # A tool to collect extremely verbose debug log from the Android system.
 # LICENSE: BSD 3-Clause by ThePedroo
 
-SAVE_FOLDER=/data/local/tmp
+SAVE_FOLDER=/data/local/tmp/DebugAssistant
+
+mkdir -p "$SAVE_FOLDER"
 
 function prepare_file() {
   local file_path="$SAVE_FOLDER/DebugAssistant$1.log"
 
+  rm "$file_path" || true
   touch "$file_path"
   chown shell:shell "$file_path"
 }
@@ -15,29 +18,36 @@ function prepare_file() {
 function watch_logcat() {
   local file_path="$SAVE_FOLDER/DebugAssistant$1.log"
 
-  logcat -f "$file_path" &
+  while [ ! ];do logcat;sleep 1;done > "$file_path" &
 }
 
-# Check if file exists
-if [ -f $SAVE_FOLDER/DebugAssistant.log ]; then
-  # Check if file -2 exists
-  if [ -f $SAVE_FOLDER/DebugAssistant-Boot2.log ]; then
-    # Check if file -3 exists
-    if [ -f $SAVE_FOLDER/DebugAssistant-Boot3.log ]; then
-      rm $SAVE_FOLDER/DebugAssistant-Boot3.log
-      rm $SAVE_FOLDER/DebugAssistant-Boot2.log
-      rm $SAVE_FOLDER/DebugAssistant.log
-
+LATEST=$(ls -t $SAVE_FOLDER | head -1)
+if [[ "$LATEST" != "" ]]; then
+  index=1
+  for f in "$SAVE_FOLDER"/; do
+    LATEST=$(ls -t "$SAVE_FOLDER" | awk NR==$index)
+    if [[ "$LATEST" == "" ]]; then
       prepare_file ""
       watch_logcat ""
-    else
-      prepare_file "-Boot3"
-      watch_logcat "-Boot3"
+      break
     fi
-  else
-    prepare_file "-Boot2"
-    watch_logcat "-Boot2"
-  fi
+    if [[ "$LATEST" == "DebugAssistant.log" ]]; then
+      prepare_file "-2"
+      watch_logcat "-2"
+      break
+    fi
+    if [[ "$LATEST" == "DebugAssistant-2.log" ]]; then
+      prepare_file "-3"
+      watch_logcat "-3"
+      break
+    fi
+    if [[ "$LATEST" == "DebugAssistant-3.log" ]]; then
+      prepare_file ""
+      watch_logcat ""
+      break
+    fi
+    index=$((index + 1))
+  done
 else
   prepare_file ""
   watch_logcat ""
